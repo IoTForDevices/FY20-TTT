6b645004671bd69ff64475e51dbed1ab63c7bd39
Maarten Struys <mstruys-work@outlook.com>
2020-02-21 12:06:24 +0100

    Finished LAB07 description

---
+++ AZ-220/LABS/LAB07-Device-Message-Routing.md
@@ -0,0 +1,48 @@
+# Device Message Routing
+Message routing enables you to send messages from your devices to cloud services in an automated, scalable, and reliable manner. Message routing can be used for:
+- Sending device telemetry messages as well as events namely, device lifecycle events, and device twin change events to built-in-endpoint and custom endpoints.
+- Filtering data before routing it to various endpoints by applying rich queries. Message routing allows you to query on the message properties and message body as well as device twin tags and device twin properties.
+## In this lab
+In this lab, you will learn to create a message route to blob storage and create another message route to an Azure Analytics job.
+## Prerequisites
+This lab assumes that you have the following resources available:
+Resource Type | Resource Name
+--------------|--------------
+Resource Group | AZ-220-RG
+IoT Hub | AZ-220-HUB-*your unique identifier*
+### Exercise 1: Create a new Device Identity and a Simulated Device
+- Inside the Azure Cloud Shell, now create a new Device Identity in your IoT Hub:
+  ```sh
+  az iot hub device-identity create --hub-name {IoTHubName} --device-id VibrationSensorId
+  az iot hub device-identity show-connection-string --hub-name {IoTHubName} --device-id VibrationSensorId --output table
+  ``` 
+You will now configure a simuated device written in C# to connect to your Azure IoT Hub using the Device ID and Shared Access Key created in exercise 1.
+  - Using Visual Studio Code, open the /LabFiles folder and the Program.cs file.
+  - Add your connection string to the Program.cs file
+  - Run the simulated device from within the Visual Studio Code terminal
+### Exercise 2: Create a Message Route to Azure Blob Storage
+he architecture of our vibration monitoring system requires data be sent to two destinations: storage and analysis. Azure IoT Hub provides a great method of directing data to the right service, through *message routing*.
+- Add a new message route to your IoT Hub to store telemetry messages, name it **vibrationLoggingRoute** and call the endpoint **vibrationLogEndpoint** and select **Pick a container** as storage location.
+  - Create a new *StorageV2 (general purpose V2)* Storage Account in your AZ-220-RG Resource Group with the name **vibrationstore-_"your unique identifier"_**
+  - Make sure to set the **Performance** field to *Standard* and the **Replication** filed to *Locally-redundant storage (LRS)*.
+  - Create a new container in the storage account you just created with the name **vibrationcontainer**. Make sure that the **Public access level** is set to *Private (no anonymous access)*.
+  - In the message routing blade, enable the newly created route and set the following query:
+    ```sql
+    sensorID = "VSLog"
+    ```
+### Exercise 3: Logging Route verifcation with Azure Stream Analytics
+To verify that the logging route is working as expected, we will create a Stream Analytics job that routes logging messages to Blob storage, which can then be validated using Storage Explorer in the Azure Portal.
+> NOTE: It may seem odd to be routing data to storage, then again sending it to storage through Azure Stream Analytics. In a production scenario, you wouldn't have both paths long-term. Instead, the second path that we're creating here would not exist. We're using it here simply as a way to demonstrate Azure Stream Analytics in an easy-to-validate way in a lab environment.
+- Create a Stream Analytics Job with the name **vibrationJob**, and with the *Streaming Units* set to **1**.
+- Create aliases for **stream input** (*vibrationInput*), **stream output** (*vibrationOutput*). Input should come from your IoT Hub, output should go to your **vibrationcontainer**. Store the data as **Line separated JSON**.
+- In the query edit pane, add the following query:
+  ```sql
+  SELECT
+    *
+  INTO
+    vibrationOutput
+  FROM
+    vibrationInput
+  ```
+- Test the logging route by looking at json data that was send by the Stream Analytics job. This data contains only telemetry messages, no logging messages.
+This concludes LAB07. If you want to have more detailed instructions for the lab (when you are installing on Windows), complete step-by-step instructions are [available here](https://github.com/MicrosoftLearning/AZ-220-Microsoft-Azure-IoT-Developer/blob/master/Instructions/Labs/LAB_AK_07-analyze-message-data-in-real-time.md).
---
+++ AZ-220/LABS/LabFiles/06-Automatic-Enrollment-with-DPS/Program.cs
@@ -0,0 +1,206 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Provisioning.Client;
+using Microsoft.Azure.Devices.Provisioning.Client.Transport;
+using Microsoft.Azure.Devices.Shared;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using System.Security.Cryptography.X509Certificates;
+
+namespace X509CertificateSimulatedDevice
+{
+    class Program
+    {
+        // ////////////////////////////////////////////////////////
+
+        // Azure Device Provisioning Service (DPS) Global Device Endpoint
+        private const string GlobalDeviceEndpoint = "global.azure-devices-provisioning.net";
+
+        // Azure Device Provisioning Service (DPS) ID Scope
+        private static string dpsIdScope = "<DPS-ID-Scope>";
+
+        // Certificate (PFX) File Name
+        private static string s_certificateFileName = "new-device.cert.pfx";
+        
+        // Certificate (PFX) Password
+        private static string s_certificatePassword = "1234";
+
+        // NOTE: For the purposes of this example, the s_certificatePassword is
+        // hard coded. In a production device, the password will need to be stored
+        // in a more secure manner. Additionally, the certificate file (PFX) should
+        // be stored securely on a production device using a Hardware Security Module.
+
+        // ////////////////////////////////////////////////////////
+
+        public static int Main(string[] args)
+        {
+            X509Certificate2 certificate = LoadProvisioningCertificate();
+
+            using (var security = new SecurityProviderX509Certificate(certificate))
+            {
+                using (var transport = new ProvisioningTransportHandlerAmqp(TransportFallbackType.TcpOnly))
+                {
+                    ProvisioningDeviceClient provClient =
+                        ProvisioningDeviceClient.Create(GlobalDeviceEndpoint, dpsIdScope, security, transport);
+
+
+                    var provisioningDeviceLogic = new ProvisioningDeviceLogic(provClient, security);
+                    provisioningDeviceLogic.RunAsync().GetAwaiter().GetResult();
+                }
+            }
+
+            return 0;
+        }
+
+        private static X509Certificate2 LoadProvisioningCertificate()
+        {
+            var certificateCollection = new X509Certificate2Collection();
+            certificateCollection.Import(s_certificateFileName, s_certificatePassword, X509KeyStorageFlags.UserKeySet);
+
+            X509Certificate2 certificate = null;
+
+            foreach (X509Certificate2 element in certificateCollection)
+            {
+                Console.WriteLine($"Found certificate: {element?.Thumbprint} {element?.Subject}; PrivateKey: {element?.HasPrivateKey}");
+                if (certificate == null && element.HasPrivateKey)
+                {
+                    certificate = element;
+                }
+                else
+                {
+                    element.Dispose();
+                }
+            }
+
+            if (certificate == null)
+            {
+                throw new FileNotFoundException($"{s_certificateFileName} did not contain any certificate with a private key.");
+            }
+
+            Console.WriteLine($"Using certificate {certificate.Thumbprint} {certificate.Subject}");
+            return certificate;
+        }
+    }
+
+
+    // The ProvisioningDeviceLogic class contains the device logic to read from the
+    // simulated Device Sensors, and send Device-to-Cloud messages to the Azure IoT
+    // Hub. It also contains the code that updates the device with changes to the
+    // Device Twin "telemetryDelay" Desired Property.
+    public class ProvisioningDeviceLogic
+    {
+        #region Constructor
+
+        readonly ProvisioningDeviceClient _provClient;
+        readonly SecurityProvider _security;
+        DeviceClient iotClient;
+
+        // Delay between Telemetry readings in Seconds (default to 1 second)
+        private int _telemetryDelay = 1;
+
+        public ProvisioningDeviceLogic(ProvisioningDeviceClient provisioningDeviceClient, SecurityProvider security)
+        {
+            _provClient = provisioningDeviceClient;
+            _security = security;
+        }
+
+        #endregion
+
+        public async Task RunAsync()
+        {
+            Console.WriteLine($"RegistrationID = {_security.GetRegistrationID()}");
+
+            // Register the Device with DPS
+            Console.Write("ProvisioningClient RegisterAsync . . . ");
+            DeviceRegistrationResult result = await _provClient.RegisterAsync().ConfigureAwait(false);
+
+            Console.WriteLine($"Device Registration Status: {result.Status}");
+            Console.WriteLine($"ProvisioningClient AssignedHub: {result.AssignedHub}; DeviceID: {result.DeviceId}");
+
+            // Verify Device Registration Status
+            if (result.Status != ProvisioningRegistrationStatusType.Assigned)
+            {
+                throw new Exception($"DeviceRegistrationResult.Status is NOT 'Assigned'");
+            }
+
+            // Create x509 DeviceClient Authentication
+            Console.WriteLine("Creating X509 DeviceClient authentication.");
+            var auth = new DeviceAuthenticationWithX509Certificate(result.DeviceId, (_security as SecurityProviderX509).GetAuthenticationCertificate());
+
+
+            Console.WriteLine("Simulated Device. Ctrl-C to exit.");
+            using (iotClient = DeviceClient.Create(result.AssignedHub, auth, TransportType.Amqp))
+            {
+                // Explicitly open DeviceClient to communicate with Azure IoT Hub
+                Console.WriteLine("DeviceClient OpenAsync.");
+                await iotClient.OpenAsync().ConfigureAwait(false);
+
+
+                // TODO 1: Setup OnDesiredPropertyChanged Event Handling to receive Desired Properties changes
+                
+
+                // TODO 2: Load Device Twin Properties since device is just starting up
+
+
+                // Start reading and sending device telemetry
+                Console.WriteLine("Start reading and sending device telemetry...");
+                await SendDeviceToCloudMessagesAsync(iotClient);
+
+                // Explicitly close DeviceClient
+                Console.WriteLine("DeviceClient CloseAsync.");
+                await iotClient.CloseAsync().ConfigureAwait(false);
+            }
+        }    
+
+        private async Task SendDeviceToCloudMessagesAsync(DeviceClient deviceClient)
+        {
+            // Initial telemetry values
+            double minTemperature = 20;
+            double minHumidity = 60;
+            double minPressure = 1013.25;
+            double minLatitude = 39.810492;
+            double minLongitude = -98.556061;
+            Random rand = new Random();
+
+            while (true)
+            {
+                double currentTemperature = minTemperature + rand.NextDouble() * 15;
+                double currentHumidity = minHumidity + rand.NextDouble() * 20;
+                double currentPressure = minPressure + rand.NextDouble() * 12;
+                double currentLatitude = minLatitude + rand.NextDouble() * 0.5;
+                double currentLongitude = minLongitude + rand.NextDouble() * 0.5;
+
+                // Create JSON message
+                var telemetryDataPoint = new
+                {
+                    temperature = currentTemperature,
+                    humidity = currentHumidity,
+                    pressure = currentPressure,
+                    latitude = currentLatitude,
+                    longitude = currentLongitude
+                };
+                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+                // Add a custom application property to the message.
+                // An IoT hub can filter on these properties without access to the message body.
+                message.Properties.Add("temperatureAlert", (currentTemperature > 30) ? "true" : "false");
+
+                // Send the telemetry message
+                await deviceClient.SendEventAsync(message);
+                Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+
+                // Delay before next Telemetry reading
+                await Task.Delay(this._telemetryDelay * 1000);
+            }
+        }
+
+    }
+}
